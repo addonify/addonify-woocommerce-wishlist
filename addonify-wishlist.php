b47a112670b53b68658f6def3796dd7c4f496dc8
Ashok Lama <crazyhat17@gmail.com>
2024-08-06 16:29:43 +0545

    Updated language file, versions and readme
    - Updated language file, versions and readme

--- addonify-wishlist.php
+++ addonify-wishlist.php
@@ -10,9 +10,9 @@
  * Plugin Name:       Addonify - WooCommerce Wishlist
  * Plugin URI:        https://wordpress.org/plugins/addonify-wishlist
  * Description:       Addonify WooCommerce Wishlist is a light-weight yet powerful tool that adds a wishlist functionality to your e-commerce shop.
- * Version:           2.0.11
+ * Version:           2.0.12
  * Requires at least: 6.3
- * Tested up to:      6.5
+ * Tested up to:      6.6
  * Requires PHP:      7.4
  * Author:            Addonify
  * Author URI:        https://www.addonify.com
@@ -28,7 +28,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'ADDONIFY_WISHLIST_VERSION', '2.0.11' );
+define( 'ADDONIFY_WISHLIST_VERSION', '2.0.12' );
 define( 'ADDONIFY_WISHLIST_DB_INITIALS', 'addonify_wishlist_' );
 define( 'ADDONIFY_WISHLIST_PLUGIN_PATH', __DIR__ );
 define( 'ADDONIFY_WISHLIST_PLUGIN_FILE', __FILE__ );
@@ -66,7 +66,9 @@ require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 require_once plugin_dir_path( __FILE__ ) . 'admin/app.php';
 
 if ( ! function_exists( 'addonify_wishlist_admin_notices' ) ) {
-
+	/**
+	 * Displays WooCommerce required admin notice.
+	 */
 	function addonify_wishlist_admin_notices() {
 
 		if (
